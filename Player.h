3b03ed865161751a1f596844f0d276301f95067e
Bryan Xu <bryanxu@ucsb.edu>
2020-09-01 17:47:46 -0700

    player.h

--- Player.h
+++ Player.h
@@ -7,31 +7,40 @@
 
 class Player {
 	public:
-		Player(std::string name, std::string gender, int age, std::string club, int phone, std::string email);
+		Player(std::string firstName, std::string lastName, std::string gender, int age, std::string club, int phone, std::string email, std::vector<std::string> events);
 		Player(const Player& orig);
 		~Player();
 
-		std::string getName() const;
+		std::string getFirstName() const;
+		std::string getLastName() const;
+		std::string getFullName() const;
 		std::string getGender() const;
 		int getAge() const;
 		std::string getClub() const;
 		int getPhone() const;
 		std::string getEmail() const;
+		std::vector<std::string> getEvents() const;
 
-		void setName(std::string name);
+		void setFirstName(std::string name);
+		void setLastName(std::string name);
 		void setGender(std::string gender);
 		void setAge(int age);
 		void setClub(std::string club);
 		void setPhone(int phone);
 		void setEmail(std::string);
-		
+		void addEvents(std::vector<std::string>);
+		void removeEvents(std::vector<std::string>); 
+
+		void printPlayer();
 	private:
-		std::string name;
+		std::string firstName;
+		std::string lastName;
 		std::string gender;
 		std::string club;
 		int phone;
 		int age;
 		std::string email;
+		std::vector<std::string> events;
 }
 
 
